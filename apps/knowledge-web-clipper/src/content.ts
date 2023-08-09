bbfb321220c3be59b5c5620f5d7af7338ae3de79
convexwf <convexwf@gmail.com>
2023-08-09 10:20:01 +0800

    fix: isolate clipper content script

--- apps/knowledge-web-clipper/src/content.ts
+++ apps/knowledge-web-clipper/src/content.ts
@@ -1,54 +1,56 @@
-const knowledgeWindow = window as typeof window & {
-  __knowledgeWebClipperContentLoaded?: boolean;
-};
-
-if (!knowledgeWindow.__knowledgeWebClipperContentLoaded) {
-  knowledgeWindow.__knowledgeWebClipperContentLoaded = true;
-
-  chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
-    if (message?.type !== "knowledge.collectSnapshot") {
-      return false;
-    }
-
-    sendResponse(collectSnapshot());
-    return true;
-  });
-}
-
-function collectSnapshot() {
-  return {
-    pageUrl: location.href,
-    canonicalUrl: document.querySelector<HTMLLinkElement>('link[rel="canonical"]')?.href,
-    title: document.title,
-    html: document.documentElement.outerHTML,
-    text: document.body?.innerText,
-    capturedAt: new Date().toISOString(),
-    meta: collectMeta(),
-    selectionHtml: collectSelectionHtml()
+(() => {
+  const knowledgeWindow = window as typeof window & {
+    __knowledgeWebClipperContentLoaded?: boolean;
   };
-}
 
-function collectMeta(): Record<string, string> {
-  const meta: Record<string, string> = {};
-  for (const item of Array.from(document.querySelectorAll<HTMLMetaElement>("meta"))) {
-    const key = item.name || item.getAttribute("property") || item.getAttribute("http-equiv");
-    const content = item.content;
-    if (key && content) {
-      meta[key] = content;
+  if (!knowledgeWindow.__knowledgeWebClipperContentLoaded) {
+    knowledgeWindow.__knowledgeWebClipperContentLoaded = true;
+
+    chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
+      if (message?.type !== "knowledge.collectSnapshot") {
+        return false;
+      }
+
+      sendResponse(collectSnapshot());
+      return true;
+    });
+  }
+
+  function collectSnapshot() {
+    return {
+      pageUrl: location.href,
+      canonicalUrl: document.querySelector<HTMLLinkElement>('link[rel="canonical"]')?.href,
+      title: document.title,
+      html: document.documentElement.outerHTML,
+      text: document.body?.innerText,
+      capturedAt: new Date().toISOString(),
+      meta: collectMeta(),
+      selectionHtml: collectSelectionHtml()
+    };
+  }
+
+  function collectMeta(): Record<string, string> {
+    const meta: Record<string, string> = {};
+    for (const item of Array.from(document.querySelectorAll<HTMLMetaElement>("meta"))) {
+      const key = item.name || item.getAttribute("property") || item.getAttribute("http-equiv");
+      const content = item.content;
+      if (key && content) {
+        meta[key] = content;
+      }
     }
-  }
-  return meta;
-}
-
-function collectSelectionHtml(): string | undefined {
-  const selection = window.getSelection();
-  if (!selection || selection.rangeCount === 0 || selection.isCollapsed) {
-    return undefined;
+    return meta;
   }
 
-  const container = document.createElement("div");
-  for (let index = 0; index < selection.rangeCount; index += 1) {
-    container.append(selection.getRangeAt(index).cloneContents());
+  function collectSelectionHtml(): string | undefined {
+    const selection = window.getSelection();
+    if (!selection || selection.rangeCount === 0 || selection.isCollapsed) {
+      return undefined;
+    }
+
+    const container = document.createElement("div");
+    for (let index = 0; index < selection.rangeCount; index += 1) {
+      container.append(selection.getRangeAt(index).cloneContents());
+    }
+    return container.innerHTML || undefined;
   }
-  return container.innerHTML || undefined;
-}
+})();
